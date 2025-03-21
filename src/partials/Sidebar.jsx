cf926693fc35c4e7d26c70bc9b9b6d90900b096f
Renren <johnlawrencemartinez05@gmail.com>
2025-03-22 06:32:22 +0800

    Update Sidebar.jsx

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { NavLink, useLocation } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 import SidebarLinkGroup from "./SidebarLinkGroup";
 
@@ -976,11 +977,16 @@ function Sidebar({
                           <li className="mb-1 last:mb-0">
                             <NavLink
                               end
-                              to="#
-                              <div className={`some-static-class ${isActive ? 'some-dynamic-class' : ''}`}>
-                               {/* ... other code ... */}
-                              </div>: "text-gray-500/90 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200")
-                              }
+                              {isActive ? (
+  <div className={`some-static-class ${isActive ? 'some-dynamic-class' : ''}`}>
+    {/* ... other code ... */}
+  </div>
+) : (
+  <Link to="#" className="text-gray-500/90 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
+    {/* your link content goes here */}
+    Link
+  </Link>
+)}
                             >
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
                                 Roadmap
